6313fef8ed3942e9d061cbf207aa696911ee871a
Vrushti Patel <vrushtip2006@gmail.com>
2026-09-19 16:32:51 +0530

    logger and exception files setup done

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,27 @@
 import sys # helps in manipulation of the python runtime environment.
+# sys.exc_info() --> This gives information about the currently handled exception.
 from src.logger import logging
 
-def error_message_detail(error,error_detail:sys): #error_detail --> will be present inside of sys
+def error_message_detail(error,error_detail:sys): #error_detail --> will be present inside of sys --> sys.exc_info()
     _,_,exc_tb=error_detail.exc_info()
-    # this will give us three infos: 
-    # we are interested in only the third info -->  on which file/which line the exception has occured and everything
-    # it'll be stored in exc_tb.
+    '''
+    this will give us three infos: type, value, traceback
 
+    For example:
+        exc_type  → ZeroDivisionError
+        exc_value → division by zero
+        exc_tb    → information about where it happened
+
+        we are interested in only the third info -->  on which file/which line the exception has occured and everything
+        it'll be stored in exc_tb.
+        
+    '''
+    
     file_name=exc_tb.tb_frame.f_code.co_filename
     # this is given in custom exception handling documentation.
-
+    # traceback --> Get the frame where the exception occurred --> Get information about the Python code being executed --> Get the filename.
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+     file_name, exc_tb.tb_lineno, str(error))
 
     return error_message
 # whenever error is raised --> we'll call this function.
@@ -19,12 +29,25 @@ def error_message_detail(error,error_detail:sys): #error_detail --> will be pres
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        super().__init__(error_message) #initialize the parent Exception class.
+        self.error_message=error_message_detail(
+            error_message, #call the function above and get the error msg
+            error_detail=error_detail #error detail --> obtained from runtime env --> sys.exc_info()
+            )
     
     def __str__(self):
         return self.error_message
-    
 
 
-        
+'''
+This is basically how the error will be passed:
+
+try:
+    x = 10 / 0
+except Exception as e:
+    error = CustomException(e, sys)
+    logging.error(error)
+    raise error
+
+this code also handles logging of errors :)
+'''
--- src/logger.py
+++ src/logger.py
@@ -3,16 +3,33 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# LOG_FILE becomes: 09_19_2026_16_02_35.log
+# Because you don't want every execution of your project to overwrite the previous log.
+# for every log (everytime, time will be different) and new log_file will be created.
+
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+# this created --> C:\Users\Vrushti\MLProject\logs\09_19_2026_16_02_35.log
+# a logs folder is created and new file is created every time inside it.
+
 os.makedirs(logs_path,exist_ok=True)
+# tries to create a directory with that entire path.
+# "If the folder doesn't exist, create it. If it already exists, that's fine.
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# final location of the log file. 
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
+    # stores logs in this file.
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    # 
     level=logging.INFO,
-
-
+    # level=logging.INFO means:
+    #  INFO, WARNING, ERROR, and CRITICAL messages are recorded, 
+    # but DEBUG messages are not.
 )
- 
+# %(asctime)s → Date and time when the log was generated.
+# %(lineno)d → Line number where the logging call happened.
+# %(name)s → Name of the logger.
+# %(levelname)s → Severity/type of the log, such as INFO, WARNING, ERROR, CRITICAL, or DEBUG.
+# %(message)s → The actual message written in the log, such as `logging.info("Training started")`.
