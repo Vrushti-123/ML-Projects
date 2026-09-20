4323a36390cdad882e881f57f1ff259b1e8f6ce5
Vrushti Patel <vrushtip2006@gmail.com>
2026-09-20 09:55:00 +0530

    Application.py created for deployment

---
+++ application.py
@@ -0,0 +1,58 @@
+'''
+This file is basically the Flask web application / frontend-backend bridge for your ML project.
+Its job is:
+User enters data in the HTML form → Flask receives it → converts 
+it into a DataFrame → sends it to your trained ML pipeline → gets 
+prediction → displays prediction on the webpage.
+'''
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+# creates your Flask web application. you're creating an object of the Flask class.
+# This object (application) will manage things like:
+# your routes (/, /predictdata, etc.)
+# incoming HTTP requests
+# returning HTML responses
+# templates, configuration etc.
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
